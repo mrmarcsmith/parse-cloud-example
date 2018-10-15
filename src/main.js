879b8c6173d3816bb3059cc64c6338a5dce072b2
Marc Smith <mr.marcsmith@me.com>
2018-10-15 15:01:21 -0700

    removed crash line

--- src/main.js
+++ src/main.js
@@ -12,6 +12,6 @@ var path = require('path');
 
 
 //Standard
-require(path.join(__dirname, '../../mmCloud/src/mmFunctions.js'))();
+// require(path.join(__dirname, '../../mmCloud/src/mmFunctions.js'))();
 require(path.join(__dirname, 'cloudFunctions.js')); //() not needed for Parse Functions
 
